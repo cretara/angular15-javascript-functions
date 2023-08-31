67f93d72d9678dad6c22a570b28a19f6c9b2bf9a
Pier Daniele Cretara <Pier Daniele Cretara>
2023-08-31 09:10:29 +0200

    Changed POC with jsFunctionCstom initialized inside ngOnInit (like after an API call)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  private jsFunctionCustom = Function(
-    '{var a = document.querySelector("#test");console.debug(a);a.style.visibility = "hidden";return a.value !== "0"}'
-  );
+  private jsFunctionCustom: Function = new Function();
 
   private jsFunctionGeneral = Function('{var a = "Hello world!"; return a}');
 
   ngOnInit(): void {
+    this.jsFunctionCustom= Function(
+      '{var a = document.querySelector("#test");console.debug(a);a.style.visibility = "hidden";return a.value !== "0"}'
+    );
     console.debug(this.jsFunctionGeneral());
     const returnValueCustomFunction = this.jsFunctionCustom();
     console.debug(returnValueCustomFunction);
