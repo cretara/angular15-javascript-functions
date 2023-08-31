0f5a20513c3bc3e1c324b595391e2f976924d595
Pier Daniele Cretara <Pier Daniele Cretara>
2023-08-31 08:41:45 +0200

    first commit with simple function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular15-javascript-functions';
+
+  private jsFunctionCustom =
+    'function custom() {var a = document.querySelector("test");return a.value !== "0"}';
+
+  private jsFunctionGeneral =
+    'function general() {var a = "Hello world!"; return a}';
+
+
+    constructor(){
+      eval(this.jsFunctionCustom);
+      eval(this.jsFunctionGeneral);
+    }
+
+  ngOnInit(): void {
+    const result = Function('{var a = "Hello world!"; return a}');
+    console.debug(result());
+  }
 }
